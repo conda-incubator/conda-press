07e704a37ce1bf8c8e5e65c4eaadccbbb8cd3a20
Anthony Scopatz <scopatz@gmail.com>
2019-04-16 18:31:08 -0400

    fixes syntax error

--- tests/test_condatools.py
+++ tests/test_condatools.py
@@ -24,7 +24,7 @@ def isexecutable(filepath):
 def test_no_symlinks(pip_install_artifact):
     # pip cannot unpack real symlinks, so insure it isn't
     wheel, test_env, sp = pip_install_artifact("re2=2016.11.01")
-    should_be_symlink = os.path.join(sp, 'lib', 'libre2' _ SO_EXT)
+    should_be_symlink = os.path.join(sp, 'lib', 'libre2' + SO_EXT)
     assert os.path.isfile(should_be_symlink)
     assert not os.path.islink(should_be_symlink)
 
