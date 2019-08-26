f44186b0f1e3ba62bdf956765182c0dd34c37a46
Anthony Scopatz <scopatz@gmail.com>
2019-08-26 17:19:22 -0400

    bumped version to 0.0.2

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def main():
         long_description=readme,
         long_description_content_type='text/markdown',
         license='BSD',
-        version='0.0.1',
+        version='0.0.2',
         author='Anthony Scopatz',
         maintainer='Anthony Scopatz',
         author_email='scopatz@gmail.com',
