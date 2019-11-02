88f08c8fb8cbed2631de7ce46af4d705c276e95e
Marcelo Duarte Trevisani <marceloduartetrevisani@gmail.com>
2019-11-02 10:59:21 +0000

    Fix when config yaml received is empty

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,7 +7,7 @@ def test_main(script_runner, data_folder):
     # Sanity test for main to see if empty options are being handled
     conda_pkg = os.path.join(data_folder, "test-deps-0.0.1-py_0.tar.bz2")
     response = script_runner.run(main.__file__, conda_pkg)
-    assert response.success
+    assert response.success, response.stderr
 
 
 def test_main_empty_config_yaml(tmpdir, script_runner, data_folder):
@@ -17,4 +17,4 @@ def test_main_empty_config_yaml(tmpdir, script_runner, data_folder):
     response = script_runner.run(
         main.__file__, conda_pkg, "--config", str(cp_yaml)
     )
-    assert response.success
+    assert response.success, response.stderr
